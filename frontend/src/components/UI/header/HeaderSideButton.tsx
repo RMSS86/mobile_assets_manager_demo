67b9da48154cb128e3a6764dc9b2217c902ba065
Robert Solis-Stevenson <98938524+RMSS86@users.noreply.github.com>
2025-07-18 20:49:41 -0600

    [ FE ] NavLinks Beautified

--- frontend/src/components/UI/header/HeaderSideButton.tsx
+++ frontend/src/components/UI/header/HeaderSideButton.tsx
@@ -27,6 +27,13 @@ export default function HeaderSideButton({
       style={_style}
     >
         <img alt='logo' src={GeneralLogo} className='side_btn__logo' onClick={_onClick}/>
+        <input type='checkbox' id='sidebar-active'/>
+
+        {/* ///CHECK BOX LOGIC/// */}
+        <label htmlFor='sidebar-active'>
+        <svg xmlns="http://www.w3.org/2000/svg" height="32px" viewBox="0 -960 960 960" width="32px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg>  
+        </label>
+        
         <a className='side_btn__text'>e-AM</a>
       {children}
     </ContextType>
